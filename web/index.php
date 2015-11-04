b76d4983a77a0269332d573c2f4ef0d2fba2c529
fiona <anna.mikulinska@gmail.com>
2015-11-04 16:54:57 +0100

    array encoded in json format echoed on the screen

--- web/index.php
+++ web/index.php
@@ -6,7 +6,12 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
+    $zmienna = array();
+    $zmienna = [0 => array('name' => 'Anna',
+    'email' => 'ghjkl@hjk.com'), 1 => array('name' => 'Kasia', 'email' => 'ghjk@hjkl.com')];
+//    return 'Hello '.$app->escape($name);
+    return $app->json($zmienna);
 });
+
 
 $app->run();
