b5f556dd1ed83fe0b3f2583af6f824ff36c7649b
krasai <krasuskip@gmail.com>
2015-11-06 15:19:49 +0100

    [TIC-20]  allegroBridge first draft

--- src/Bridges/AllegroBridge.php
+++ src/Bridges/AllegroBridge.php
@@ -9,9 +9,42 @@
 namespace Tickets\Bridges;
 
 
+use Tickets\Connectors\AllegroConnector;
+use Tickets\Models\Ticket;
+
 class AllegroBridge
 {
-public function getSportTickers(){}
+    private $rawAllegroTickets;
+    private $tickets = [];
 
-public function getConcertTickers(){}
+    public function getSportTickers(){
+        $query = 'getSportTickets';
+        $this->rawAllegroTickets = $this->createRawAllegroTickets($query);
+        return $this->transformToTicketObjects($this->rawAllegroTickets);
+    }
+
+    public function getConcertTickers(){
+        $query = 'getConcertTickets';
+        $this->rawAllegroTickets = $this->createRawAllegroTickets($query);
+        return $this->transformToTicketObjects($this->rawAllegroTickets);
+    }
+
+    private function createRawAllegroTickets($params){
+        $allegroConnect =  new AllegroConnector();
+        return $allegroConnect->call('GET', $params);
+    }
+
+    private function transformToTicketObjects ($rawAllegroTickets) {
+        foreach ($rawAllegroTickets as $rawTicket) {
+            $ticket =  new Ticket();
+            $ticket->title = $rawTicket['ticketTitle'];
+            $ticket->auctionUrl = $rawTicket['ticketAuctionUrl'];
+            $ticket->description = $rawTicket['ticketDescription'];
+            $ticket->price = $rawTicket['ticketPrice'];
+            $this ->tickets[] = $ticket;
+        }
+        return $this->tickets;
+    }
+
+
 }
