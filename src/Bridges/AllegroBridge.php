093f1514414f332087cfed91feeb0896f976d3a1
krasai <krasuskip@gmail.com>
2015-11-09 16:55:25 +0100

    [TIC-20]  allegroBridge - tests run correctly

--- src/Bridges/AllegroBridge.php
+++ src/Bridges/AllegroBridge.php
@@ -27,20 +27,24 @@ class AllegroBridge
     }
 
     public function getSportTickets(){
-        $query = 'getSportTickets';
+        $query = 'sport';
         $this->rawAllegroTickets = $this->createRawAllegroTickets($query);
         if (isset($this->rawAllegroTickets['error']) === true && empty($this->rawAllegroTickets['error']) === false ) {
-            return $this->tickets['error'] = 1;
+            return $this->tickets = ['error' => 1];
+        } else if (empty($this->rawAllegroTickets) === true ) {
+            return null;
         } else {
             return $this->transformToTicketObjects($this->rawAllegroTickets);
         }
     }
 
     public function getConcertTickets(){
-        $query = 'getConcertTickets';
+        $query = 'concert';
         $this->rawAllegroTickets = $this->createRawAllegroTickets($query);
         if (isset($this->rawAllegroTickets['error']) === true && empty($this->rawAllegroTickets['error']) === false ) {
-            return $this->tickets['error'] = 1;
+            return $this->tickets = ['error' => 1];
+        } else if (empty($this->rawAllegroTickets) === true ) {
+            return null;
         } else {
             return $this->transformToTicketObjects($this->rawAllegroTickets);
         }
@@ -52,16 +56,19 @@ class AllegroBridge
     }
 
     private function transformToTicketObjects ($rawAllegroTicketsArr) {
+        $rawAllegroTicketsArr = json_decode(json_encode($rawAllegroTicketsArr),true);
         foreach ($rawAllegroTicketsArr as $rawTicket) {
             $ticket =  new Ticket();
-            $ticket->title = $rawTicket['ticketTitle'];
-            $ticket->auctionUrl = $rawTicket['ticketAuctionUrl'];
-            $ticket->description = $rawTicket['ticketDescription'];
-            $ticket->price = $rawTicket['ticketPrice'];
+            $ticket->title = $rawTicket['itemInfo']['itName'];
+            $ticket->auctionUrl = 'http://allegro.pl/show_item.php?item=' .$rawTicket['itemInfo']['itId'];
+            $ticket->description = $rawTicket['itemInfo']['itDescription'];
+            $ticket->price = $rawTicket['itemInfo']['itPrice'];
             $this ->tickets[] = $ticket;
         }
+
         return $this->tickets;
     }
+
 
 
 }
--- src/Bridges/AllegroBridgeTest.php
+++ src/Bridges/AllegroBridgeTest.php
@@ -7,10 +7,53 @@
  */
 
 namespace Tickets\Bridges;
-
+use Tickets\Models\Ticket;
 
 class AllegroBridgeTest extends \PHPUnit_Framework_TestCase
 {
+
+    public function testGetSportTickets()
+    {
+        // given
+
+        $ticketMock[] =
+            array( 'itemInfo' =>
+                array(
+                        'itId' => 5762155520,
+                        'itName' => 'Bilety Polska Czechy Bilet Wrocław TANIO HIT',
+                        'itPrice' => 15,
+                        'itDescription' => 'description',
+                        'auctionUrl' => '',
+                )
+            );
+        $ticketMock[] =
+            array( 'itemInfo' =>
+                array(
+                        'itId' => 5762155520,
+                        'itName' => 'TANIO HIT',
+                        'itPrice' => 231,
+                        'itDescription' => 'description',
+                        'auctionUrl' => 'http://allegro.pl',
+                )
+            );
+
+        $connector = $this->getMock('Tickets\Connectors\AllegroConnector');
+        $connector->expects($this->any())->method('getItems')->will($this->returnValue($ticketMock));
+        $instance = new AllegroBridge($connector);
+
+
+
+        // when
+        $result = $instance->getSportTickets();
+        $ticket = new Ticket();
+        $ticket->title = 'Bilety Polska Czechy Bilet Wrocław TANIO HIT';
+        $ticket->description = 'description';
+        $ticket->price = 15;
+        $ticket->auctionUrl = 'http://allegro.pl/show_item.php?item=5762155520';
+
+        // then
+        $this->assertEquals($ticket, $result[0]);
+    }
 
     public function testGetSportTicketsWhenConnectionFailed()
     {
@@ -30,7 +73,7 @@ class AllegroBridgeTest extends \PHPUnit_Framework_TestCase
     {
         // given
         $connector = $this->getMock('Tickets\Connectors\AllegroConnector');
-        $connector->expects($this->any())->method('getItems')->will($this->returnValue([]));
+        $connector->expects($this->any())->method('getItems')->will($this->returnValue(null));
         $instance = new AllegroBridge($connector);
 
         // when
@@ -38,6 +81,50 @@ class AllegroBridgeTest extends \PHPUnit_Framework_TestCase
 
         // then
         $this->assertNull($result);
+    }
+
+
+    public function testGetConcertTickets()
+    {
+        // given
+
+        $ticketMock[] =
+            array( 'itemInfo' =>
+                array(
+                        'itId' => 5781922684,
+                        'itName' => 'Bilety Polska Czechy Bilet Wrocław TANIO HIT',
+                        'itPrice' => 0,
+                        'itDescription' => 'description',
+                        'auctionUrl' => '',
+                )
+            );
+        $ticketMock[] =
+            array( 'itemInfo' =>
+                array(
+                    'itId' => 5780806569,
+                    'itName' => 'TANIO HIT',
+                    'itPrice' => 231,
+                    'itDescription' => 'description',
+                    'auctionUrl' => 'http://allegro.pl',
+                )
+            );
+
+        $connector = $this->getMock('Tickets\Connectors\AllegroConnector');
+        $connector->expects($this->any())->method('getItems')->will($this->returnValue($ticketMock));
+        $instance = new AllegroBridge($connector);
+
+
+
+        // when
+        $result = $instance->getConcertTickets();
+        $ticket = new Ticket();
+        $ticket->title = 'Bilety Polska Czechy Bilet Wrocław TANIO HIT';
+        $ticket->description = 'description';
+        $ticket->price = 0;
+        $ticket->auctionUrl = 'http://allegro.pl/show_item.php?item=5781922684';
+
+        // then
+        $this->assertEquals($ticket, $result[0]);
     }
 
     public function testGetConcertTicketsWhenConnectionFailed()
@@ -52,13 +139,14 @@ class AllegroBridgeTest extends \PHPUnit_Framework_TestCase
 
         // then
         $this->assertEquals(['error'=> 1], $result);
+
     }
 
     public function testGetConcertTicketsWhenThereIsNoTickets()
     {
         // given
         $connector = $this->getMock('Tickets\Connectors\AllegroConnector');
-        $connector->expects($this->any())->method('getItems')->will($this->returnValue([]));
+        $connector->expects($this->any())->method('getItems')->will($this->returnValue(null));
         $instance = new AllegroBridge($connector);
 
         // when
